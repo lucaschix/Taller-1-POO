e4c12f04666b6ed2cb6b7f12259aaa6de8acd770
lucaschix <l.colomera01@ufromail.cl>
2024-08-21 10:36:21 -0400

    avance 1 avance2 avance 3

--- .idea/JuegoDeCartas.java
+++ .idea/JuegoDeCartas.java
@@ -1,53 +1,68 @@
 import java.util.Scanner;
 
-public class JuegoDeCartas {
-    private Carta[][] matrizCartas;
-    private int numCartas;
+public JuegoDeCartas() {
+    matrizCartas = new Carta[1][3]; // Solo 3 cartas
+    numCartas = 0;
+}
 
-    public JuegoDeCartas() {
-        matrizCartas = new Carta[5][10];
-        numCartas = 0;
+public void crearMatrizCartas() {
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrizCartas[i][j] = null;
+        }
     }
+}
 
-    public void crearMatrizCartas() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 10; j++) {
-                matrizCartas[i][j] = null;
+public void agregarCartas(String palo, String valor, int puntos) {
+    Carta carta = new Carta(palo, valor, puntos);
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matrizCartas[i][j] == null) {
+                matrizCartas[i][j] = carta;
+                numCartas++;
+                return;
             }
         }
     }
+}
 
-    public void agregarCartas(String palo, String valor, int puntos) {
-        Carta carta = new Carta(palo, valor, puntos);
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (matrizCartas[i][j] == null) {
-                    matrizCartas[i][j] = carta;
-                    numCartas++;
-                    return;
-                }
+public void inicializarCartasJuego() {
+    agregarCartas("Corazones", "1", 1);
+    agregarCartas("Diamantes", "2", 2);
+    agregarCartas("Treboles", "3", 3);
+}
+
+public Carta[] obtenerCartas() {
+    Carta[] cartas = new Carta[numCartas];
+    int indice = 0;
+    for (int i = 0; i < matrizCartas.length; i++) {
+        for (int j = 0; j < matrizCartas[i].length; j++) {
+            if (matrizCartas[i][j] != null) {
+                cartas[indice] = matrizCartas[i][j];
+                indice++;
             }
         }
     }
-
-    public void inicializarCartasJuego() {
-        agregarCartas("Corazones", "As", 1);
-        agregarCartas("Diamantes", "2", 2);
-        agregarCartas("Treboles", "3", 3);
-        agregarCartas("Picas", "4", 4);
-        agregarCartas("Corazones", "5", 5);
-        agregarCartas("Diamantes", "6", 6);
-        agregarCartas("Treboles", "7", 7);
-        agregarCartas("Picas", "8", 8);
-        agregarCartas("Corazones", "9", 9);
-        agregarCartas("Diamantes", "10", 10);
-        agregarCartas("Treboles", "J", 10);
-        agregarCartas("Picas", "Q", 10);
-        agregarCartas("Corazones", "K", 10);
+    return cartas;
+}
+    public void jugar() {
+        Carta[] cartas = obtenerCartas();
+        int puntos = 0;
+        for (Carta carta : cartas) {
+            puntos += carta.getPuntos();
+        }
+        System.out.println("Puntos: " + puntos);
+        if (puntos == 2.0) {
+            System.out.println("¡Has ganado!");
+        } else if (puntos > 2.0) {
+            System.out.println("¡Has perdido! Tu puntaje es mayor a 20.");
+        } else {
+            System.out.println("¡Has perdido! Tu puntaje es menor a 20.");
+        }
     }
     public void salir (){
         System.out.println("Saliendo");
-        System.exit( O);
+        System.exit(1);
     }
     public void menu (){
         Scanner scanner = new Scanner(System.in);
@@ -57,7 +72,7 @@ public class JuegoDeCartas {
             System.out.println("1) Jugar.");
             System.out.println("2) Salir");
             System.out.println("Solo hay dos opciones: ");
-            opcion = Scanner.nextInt();
+            opcion = scanner.nextInt();
             switch (opcion){
                 case 1:
                     jugar();
@@ -66,10 +81,10 @@ public class JuegoDeCartas {
                     salir();
                     System.out.println("Adios");
                 default:
-                    System.out.println("tu opcion no es valida")
+                    System.out.println("tu opcion no es valida");
             }
-        }
-    }while (opcion != 2);
+        }while(opcion != 2);
+    }
 
     public static void main(String[] args) {
         JuegoDeCartas juego = new JuegoDeCartas();
@@ -78,4 +93,26 @@ public class JuegoDeCartas {
         juego.menu();
     }
 }
+class Carta{
+    private String tipo;
+    private String valor;
+    private int puntos;
 
+    public Carta(String tipo, String valor, int puntos) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.puntos = puntos;
+    }
+
+    public String getPalo() {
+        return tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+}
